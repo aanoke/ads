5bb299ff2871b74f91f9f188f3e17527b1467338
aanoke <73918023+aanoke@users.noreply.github.com>
2024-05-19 20:45:58 +0700

    Create terra.js

---
+++ terra.js
@@ -0,0 +1,9 @@
+	atOptions = {
+		'key' : '2fad86e3e80e75479e17f495b7e47ac6',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//italianbeepimpediment.com/2fad86e3e80e75479e17f495b7e47ac6/invoke.js">
